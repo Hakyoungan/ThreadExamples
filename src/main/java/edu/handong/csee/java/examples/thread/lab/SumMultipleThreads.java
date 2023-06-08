2751c5320066c9999a621c1e812bd1f6095299dc
Hakyoungan <hakyoung8461@gmail.com>
2023-06-08 13:29:15 +0900

    update change

--- src/main/java/edu/handong/csee/java/examples/thread/lab/SumMultipleThreads.java
+++ src/main/java/edu/handong/csee/java/examples/thread/lab/SumMultipleThreads.java
@@ -10,17 +10,6 @@ public class SumMultipleThreads {
 		ArrayList<SumRunner> sumRunners = new ArrayList<SumRunner>();
 		ArrayList<Thread> threadsForSubSum = new ArrayList<Thread>();
 
-		/* Let a thread compute a sub-sum.
-		 * sumRunners.add(new SumMultipleThreads(1,1000000));
-		 * sumRunners.add(new SumMultipleThreads(1000001,2000000));
-		 * sumRunners.add(new SumMultipleThreads(2000001,3000000));
-		 * sumRunners.add(new SumMultipleThreads(3000001,4000000));
-		 * sumRunners.add(new SumMultipleThreads(4000001,5000000));
-		 * sumRunners.add(new SumMultipleThreads(5000001,6000000));
-		 * sumRunners.add(new SumMultipleThreads(6000001,7000000));
-		 * sumRunners.add(new SumMultipleThreads(7000001,8000000));
-		 * sumRunners.add(new SumMultipleThreads(8000001,9000000));
-		 * sumRunners.add(new SumMultipleThreads(9000001,10000000));*/
 		for(long i=0; i<to/1000000; i++) {
 			SumRunner currentRunner = new SumRunner((i*1000000)+1, (i+1)*1000000);
 			sumRunners.add(currentRunner);
@@ -30,6 +19,14 @@ public class SumMultipleThreads {
 			threadsForSubSum.add(thread);
 			System.out.println("Thread-" + i + " started!");
 		}
+		for(Thread t: threadsForSubSum) {
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+
 
 		long grandTotal = 0;
 		for(SumRunner runner:sumRunners) {
